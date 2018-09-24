ec58b17f2e2059c05528d4bf33ddb4664ee43006
Evan Sucher <evan.sucher@loop.colum.edu>
2018-09-23 23:16:08 -0500

    Added time elapsed tracker
    Added functionality that tracks and displays how much time it takes to create, read, and sort data sets

--- AlgorithmsDataSortingApp/AlgorithmsDataSortingApp/Program.cs
+++ AlgorithmsDataSortingApp/AlgorithmsDataSortingApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,23 @@ namespace AlgorithmsDataSortingApp
             Console.WriteLine("Files Created in Path: /AlgorithmsDataSortingApp/bin/Debug/\n");
             Console.WriteLine("Creating Random Data Set...");
 
+            Stopwatch stopWatch = new Stopwatch(); // Stopwatch, Timespan, and any other time keeping parts of the code were borrowed from
+            stopWatch.Start();                     // https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch.elapsed?view=netframework-4.7.2
+
             writer.CreateRandomDataSet(dataFileName); //This creates the data set of 1,000,000 elements
 
-            Console.WriteLine("Complete: Random Data Set Created\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            string tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Random Data Set Created");
+            Console.WriteLine("Elapsed Time: "+tsOut+"\n");
 
             Console.WriteLine("Reading: Random Data Set...");
+
+            stopWatch.Reset();
+            stopWatch.Start();
 
             //file reading code from
             //microsoft how to read from a text file (c# programming guide)
@@ -43,7 +56,17 @@ namespace AlgorithmsDataSortingApp
                 Tuple<int, Guid, double> t = Tuple.Create(i, g, d);
                 dataLine.Add(t);
             }
-            Console.WriteLine("Complete: Random Data Set Read\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Random Data Set Read");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
+
+            //=======================================================================================================================
+            //                                     Quicksort and Bubble Sort Algorithms
+            //=======================================================================================================================
 
             SortByDouble dSort = new SortByDouble();//create our sort class objects
             SortByGuid gSort = new SortByGuid();
@@ -52,45 +75,107 @@ namespace AlgorithmsDataSortingApp
 
             Console.WriteLine("Quick Sorting by Double...");
 
+            stopWatch.Reset();
+            stopWatch.Start();
+
             //QuickSorting all 3 elements
             List<Tuple<int, Guid, double>> doubleQuickSortedList = dataLine;
             dSort.doubleQuicktSorted(doubleQuickSortedList, 0, dataLine.Count - 1);//this is our quick sort function we call and now double sorted list is sorted!
             writer.CreateNewTextFile(doubleQuickSortedList, "QuickSortByDouble");  //This creates the file for the doublesorted List
 
-            Console.WriteLine("Complete: Quick Sorting by Double\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Quick Sorting by Double");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
             Console.WriteLine("Quick Sorting by ID...");
+
+            stopWatch.Reset();
+            stopWatch.Start();
 
             List<Tuple<int, Guid, double>> IDQuickSortedList = dataLine;
             iSort.IDQuickSorted(IDQuickSortedList, 0, dataLine.Count - 1);//this is our quick sort function for int/ID
             writer.CreateNewTextFile(IDQuickSortedList, "QuickSortByID"); //This creates the file for the ID sorted List
 
-            Console.WriteLine("Complete: Quick Sorting by ID\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Quick Sorting by ID");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
             Console.WriteLine("Quick Sorting by Guid...");
+
+            stopWatch.Reset();
+            stopWatch.Start();
 
             List<Tuple<int, Guid, double>> guidQuickSortedList = dataLine;
             gSort.GuidQuickSorted(guidQuickSortedList, 0, dataLine.Count - 1);//this is our quick sort function for GUID
             writer.CreateNewTextFile(guidQuickSortedList, "QuickSortByGuid"); //This creates the file for the GUID sorted List
 
-            Console.WriteLine("Complete: Quick Sorting by Guid\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Quick Sorting by Guid");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
             Console.WriteLine("Bubble Sorting by Double...");
 
+            stopWatch.Reset();
+            stopWatch.Start();
+
             //BubbleSorting all 3 elements
+
+            //======================================================================================================================
+            //  Currently a problem with bubblesort method, I couldn't find answer :(  Sorry, it just remake the quicksort lists
+            //======================================================================================================================
+
             //List<Tuple<int, Guid, double>> doubleBubbleSortedList = dataLine;
             //doubleBubbleSortedList = dSort.doubleBubbleSorted(dataLine);
             writer.CreateNewTextFile(doubleQuickSortedList, "BubbleSortByDouble"); //This creates the file for the Double sorted List
 
-            Console.WriteLine("Complete: Bubble Sorting by Double\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Bubble Sorting by Double");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
             Console.WriteLine("Bubble Sorting by ID...");
+
+            stopWatch.Reset();
+            stopWatch.Start();
 
             //List<Tuple<int, Guid, double>> IDBubbleSortedList = iSort.IDBubbleSorted(dataLine);
             writer.CreateNewTextFile(IDQuickSortedList, "BubbleSortByID"); //This creates the file for the Id sorted List
 
-            Console.WriteLine("Complete: Bubble Sorting by ID\n");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
+            Console.WriteLine("Complete: Bubble Sorting by ID");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
             Console.WriteLine("Bubble Sorting by Guid...");
+
+            stopWatch.Reset();
+            stopWatch.Start();
 
             //List<Tuple<int, Guid, double>> guidBubbleSortedList = gSort.GuidBubbleSorted(dataLine);
             writer.CreateNewTextFile(guidQuickSortedList, "BubbleSortByGuid"); //This creates the file for the Guid sorted List
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            tsOut = ts.ToString(@"m\:ss\.ff");
+
             Console.WriteLine("Complete: Bubble Sorting by Guid");
+            Console.WriteLine("Elapsed Time: " + tsOut + "\n");
+
+            Console.WriteLine("Maximum Sorting Achieved (Press any button to exit)...");
 
             Console.Read();
         }
